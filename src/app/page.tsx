6f2856927f3b3f94c4ae550415b303205d6f79a4
sasensi <asensi.samuel@gmail.com>
2025-01-29 18:17:34 +0100

    chore: remove tailwind classes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <Link
-      className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
       href="/new"
     >
       Go to Memory Test Page
